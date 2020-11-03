a93b99e43be374ffa44058bf82916689d02d31b5
sdsugathapala <sdsugathapala@gmail.com>
2020-11-03 14:15:32 +0530

    i

---
+++ ToastLibrary/src/main/java/com/sdsugathapala/toastlibrary/ToastMessage.java
@@ -0,0 +1,10 @@
+package com.sdsugathapala.toastlibrary;
+
+import android.content.Context;
+import android.widget.Toast;
+
+public class ToastMessage {
+    public static void show(Context context, String msg) {
+        Toast.makeText(context, msg, Toast.LENGTH_SHORT).show();
+    }
+}
